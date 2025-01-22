b9eb7a4f9fac50bec746800c917d1812b623f8ef
zeynepanl <zeynepanl29@gmanil.com>
2025-01-22 18:10:00 +0300

    response comment

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -6,9 +6,10 @@ const authenticateToken = require("../lib/auth");  // JWT doğrulama middleware
 // Yorum ekleme
 router.post("/add", authenticateToken, async (req, res) => {
   try {
-    const { blog_post, user, content } = req.body;
+    const { blog_post, user, content, parent_comment } = req.body;
 
-    const newComment = new Comment({ blog_post, user, content });
+    // Eğer yanıt verilmişse, parent_comment parametresini kontrol et
+    const newComment = new Comment({ blog_post, user, content, parent_comment });
     await newComment.save();
 
     res.status(201).json({ message: "Comment added successfully." });
@@ -20,7 +21,9 @@ router.post("/add", authenticateToken, async (req, res) => {
 // Belirli bir blog yazısına ait yorumları getirme
 router.get("/:blogPostId", async (req, res) => {
   try {
-    const comments = await Comment.find({ blog_post: req.params.blogPostId }).populate("user");
+    const comments = await Comment.find({ blog_post: req.params.blogPostId })
+                                  .populate("user")
+                                  .populate("parent_comment"); // Yanıtların da parent_comment ile birlikte dönebilmesi için
     res.status(200).json(comments);
   } catch (error) {
     res.status(500).json({ error: error.message });
